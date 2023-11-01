4bb447a01bede588e24df7e67266e83ecac59017
laluoki <laluocky450@gmail.com>
2023-11-01 20:49:03 +0700

    [oki] split function for handle client and client handler for use module threading

--- tryHard/server.py
+++ tryHard/server.py
@@ -1,29 +1,36 @@
 import socket
-import random
+import threading
 
-server_ip = '192.168.88.1'
-server_port = 5000
+server_ip = "192.168.88.1"
+server_port = 4000
+
+addr = (server_ip, server_port)
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((server_ip, server_port))
+server_socket.bind(addr)
 server_socket.listen(5)
+print(f"Server mendengarkan di {addr}")
 
-print('Node is running and waiting for connections...')
 
-while True:
-    try:
-        client_socket, client_address = server_socket.accept()
-        print('Connected to client:', client_address)
-
-        message = client_socket.recv(1024).decode()
-        print('Received data:', message)
-
+def handle_client(client_socket):
+    while True:
+        data = client_socket.recv(1024).decode()
+        if not data:
+            break
+        print("Menerima data:", data)
         response = 'Node received the data'
         client_socket.send(response.encode())
-
-    except:
-        if (client_socket.close):
-            print("Connection has been stopped")
-            print("Error while receive data")
-
     client_socket.close()
+
+
+def client_handler():
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print("Menerima koneksi dari", client_address)
+        client_thread = threading.Thread(
+            target=handle_client, args=(client_socket,))
+        client_thread.start()
+
+
+if __name__ == "__main__":
+    client_handler()
